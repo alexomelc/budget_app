270a57c19a16ac62019e276381bf19d7489c7390
oom@ciklum.com <Omsa02051988>
2018-03-16 20:07:51 +0200

    add architecture

--- app.js
+++ app.js
@@ -6,34 +6,62 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputAdd: '.add__btn'
+    };
+
     return {
         getInput: function () {
             return {
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             }
+		},
 
-
+		getDOMstrings: function () {
+           return DOMstrings;
 		}
+
     }
 
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function() {
 
+    let setupEventListeners = function () {
+		let DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputAdd).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function() {
+			if(event.keyCode === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
+
+
+
+
+    let ctrlAddItem = function() {
+
+        let input = UICtrl.getInput();
+        console.log(input);
+
+    };
+
+    return {
+        init: function () {
+            console.log('aapp run');
+            setupEventListeners();
+		}
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function() {
-        
-        if(event.keyCode === 13) {  
-            ctrlAddItem();
-        }
-    });
 
 })(budgetController, UIController); 
  
