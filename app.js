86cefe5f09c5b6257f2df11aaf71aef1bb50d9eb
oom@ciklum.com <Omsa02051988>
2018-04-20 13:36:03 +0300

    new changes

--- app.js
+++ app.js
@@ -1,6 +1,53 @@
 var budgetController = (function() {
 
+	let Expense = function (id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
+	let Income = function (id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	let data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: [],
+			inc: []
+		}
+	};
+	
+	return {
+		addItem: function (type, des, val) {
+			let newItem, ID;
+
+			if(data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length-1].id + 1;
+			} else {
+				ID = 0;
+			}
+
+			if(type === 'exp') {
+				newItem = new Expense(ID, des, val);
+			} else if(type === 'inc') {
+				newItem = new Income(ID, des, val)
+			}
+
+			data.allItems[type].push(newItem);
+
+			return newItem;
+		},
+
+		testing: function () {
+			console.log(data)
+		}
+	}
 
 })();
 
@@ -53,6 +100,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         let input = UICtrl.getInput();
         console.log(input);
 
+        let newItem =  budgetCtrl.addItem(input.type, input.description, input.value)
+
     };
 
     return {
@@ -64,4 +113,6 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
 })(budgetController, UIController); 
- 
+
+
+controller.init();
